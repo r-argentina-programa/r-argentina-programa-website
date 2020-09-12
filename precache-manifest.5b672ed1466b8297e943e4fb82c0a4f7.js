1e0e5a3908340c51b4dfac9718e7ebded4dded97
vmalvaro <alvarovm@gmail.com>
2020-09-12 06:44:34 +0000

    Deploying to gh-pages from  @ 89e87616fba470bb339c09ecf06d0692538966c4 🚀

---
+++ precache-manifest.5b672ed1466b8297e943e4fb82c0a4f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bfe83bad68a4cd34025a23596aeed97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec61e4e47dc1499f1790",
+    "url": "/static/css/main.5223903d.chunk.css"
+  },
+  {
+    "revision": "c83d3fe0a5b2b6f7f20b",
+    "url": "/static/js/2.1617f136.chunk.js"
+  },
+  {
+    "revision": "943aec86c35bb3f39d18124af76a452f",
+    "url": "/static/js/2.1617f136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec61e4e47dc1499f1790",
+    "url": "/static/js/main.4f3a8c85.chunk.js"
+  },
+  {
+    "revision": "93c217ebf0c2db4f6a21",
+    "url": "/static/js/runtime-main.80faec55.js"
+  },
+  {
+    "revision": "b70109da8b4b7eae306690c78f1cb5c2",
+    "url": "/static/media/logo.b70109da.jpg"
+  }
+]);
